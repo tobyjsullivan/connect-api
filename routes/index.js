0c1cd5e1ecccf22654c883776e392c570b971d77
Toby Sullivan <tobyjsullivan@gmail.com>
2015-08-04 23:25:41 -0700

    Add initial POST /people endpoint

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/people', function(req, res, next) {
+  var person = { personId: "some-id", name: "Person1" };
+  res.json(person);
+});
+
 module.exports = router;
