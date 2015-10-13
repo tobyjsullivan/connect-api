899f61286849a206aa413eeab576984af85d3479
Toby Sullivan <tobyjsullivan@gmail.com>
2015-10-12 22:58:22 -0700

    Add authKeys endpoint and tie keys to people

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,11 @@
+var _ = require('underscore');
 var express = require('express');
 var uuid = require('node-uuid');
 
 var router = express.Router();
+
+var validKeys = [];
+var people = [];
 
 /* GET home page. */
 var statusController = function(req, res, next) {
@@ -9,17 +13,61 @@ var statusController = function(req, res, next) {
 
   var statuses = [currentStatus];
   res.json(statuses);
-}
+};
+
+var getAuthKey = function(req) {
+  if (req === undefined) {
+    throw Error("Expected arg req");
+  }
+
+  if (req.headers === undefined) {
+    throw Error("Could not find headers obj. Are you sure req is a request object?");
+  }
+
+  var authHeader  = req.headers['x-authentication'];
+
+  if (authHeader == undefined) {
+    console.log(req.headers);
+    throw Error("Header 'X-Authentication' must be supplied for this request")
+  }
+
+  if (!_.contains(validKeys, authHeader)) {
+    console.log(validKeys);
+    throw Error("That key is invalid.");
+  }
+
+  return authHeader;
+};
 
 router.get('/', statusController);
 
 router.get('/statuses', statusController);
 
-router.post('/people', function(req, res, next) {
-  var id = uuid.v4();
-  var name = "Person"+id.substring(0,6);
+router.post('/authkeys', function(req, res, next) {
+  var newKey = uuid.v4();
 
-  var person = { personId: id, name: name };
+  validKeys.push(newKey);
+
+  var key = { authKey: newKey };
+  res.json(key);
+});
+
+
+router.post('/people', function(req, res, next) {
+  var key = getAuthKey(req);
+
+  // Look for an existing person
+  var person = _.find(people, function(e) {
+    return e.key == key;
+  });
+
+  if (person === undefined) {
+    var id = uuid.v4();
+    var name = "Person_"+id.substring(0,6);
+
+    person = { personId: id, name: name, key: key };
+    people.push(person);
+  }
   res.json(person);
 });
 
